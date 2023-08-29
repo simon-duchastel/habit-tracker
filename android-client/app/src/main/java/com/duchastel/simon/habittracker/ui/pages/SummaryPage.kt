35eda657f51896ae408dbe4a559f9fd998f8f929
Simon Duchastel <simon@duchastel.com>
2023-08-28 22:19:00 -0700

    Flesh out lazy column UI

--- android-client/app/src/main/java/com/duchastel/simon/habittracker/ui/pages/SummaryPage.kt
+++ android-client/app/src/main/java/com/duchastel/simon/habittracker/ui/pages/SummaryPage.kt
@@ -1,31 +1,36 @@
 package com.duchastel.simon.habittracker.ui.pages
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.duchastel.simon.habittracker.network.models.HabitSummary
 import com.duchastel.simon.habittracker.ui.theme.HabitSummaryCompleteStrong
 import com.duchastel.simon.habittracker.ui.theme.HabitSummaryCompleteWeak
 import com.duchastel.simon.habittracker.ui.theme.HabitSummaryIncompleteStrong
 import com.duchastel.simon.habittracker.ui.theme.HabitSummaryIncompleteWeak
 import com.duchastel.simon.habittracker.utils.blendColors
 import com.duchastel.simon.habittracker.viewmodels.SummaryPageViewModel
+import com.duchastel.simon.habittracker.viewmodels.SummaryPageViewModel.*
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -41,36 +46,59 @@ fun SummaryPage(viewModel: SummaryPageViewModel = koinViewModel()) {
 }
 
 @Composable
-fun SummaryView(habits: List<HabitSummary>) {
-    Column(modifier = Modifier
+fun SummaryView(habits: List<HabitListElements>) {
+    LazyColumn(modifier = Modifier
         .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    ) {
-        Row(modifier = Modifier.fillMaxWidth(0.90f),
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-            repeat(7) { i ->
-                val size = (LocalConfiguration.current.screenWidthDp / 8).dp
+        .wrapContentSize(Alignment.Center),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
 
-                val iPrime = i + 1
-                HabitSummaryBox(size = size, completed = iPrime, uncompleted = 7 - iPrime)
+    ) {
+        items(habits) { habitElement ->
+            when (habitElement) {
+                is HabitListElements.MonthLabel -> MonthLabel(text = habitElement.month)
+                is HabitListElements.Week -> HabitSummaryRow(habits = habitElement.habits)
             }
         }
     }
 }
 
 @Composable
-fun HabitSummaryBox(size: Dp = 40.dp, completed: Int, uncompleted: Int) {
-    val total = completed + uncompleted
-    val midpoint = total / 2f
+fun MonthLabel(text: String) {
+    Text(text)
+}
 
-    val color = if (completed >= midpoint) {
-        // the further above the midpoint you get (closer to total), the stronger the complete color
-        val percentage = (completed - midpoint) / midpoint
+@Composable
+fun HabitSummaryRow(habits: List<HabitStatus>) {
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        val size = (LocalConfiguration.current.screenWidthDp / 8).dp
+        habits.forEach { habit ->
+            when (habit) {
+                is HabitStatus.Empty -> EmptyHabitSummaryBox(size = size)
+                is HabitStatus.Complete -> HabitSummaryBox(
+                    completed = true,
+                    percentage = habit.percent,
+                    size = size) { }
+                is HabitStatus.Incomplete -> HabitSummaryBox(
+                    completed = false,
+                    percentage = habit.percent,
+                    size = size) { }
+            }
+        }
+    }
+}
+
+@Composable
+fun HabitSummaryBox(
+    completed: Boolean,
+    percentage: Float,
+    size: Dp = 40.dp,
+    onClick: () -> Unit,
+) {
+    val color = if (completed) {
         blendColors(HabitSummaryCompleteWeak, HabitSummaryCompleteStrong, percentage)
     } else {
-        // the further below the midpoint you get (closer to 0), the stronger the incomplete color
-        val percentage = completed / midpoint
         blendColors(HabitSummaryIncompleteStrong, HabitSummaryIncompleteWeak, percentage)
     }
 
@@ -79,5 +107,16 @@ fun HabitSummaryBox(size: Dp = 40.dp, completed: Int, uncompleted: Int) {
         .padding(4.dp)
         .clip(RectangleShape)
         .background(color)
+        .clickable(onClick = onClick)
+    )
+}
+
+@Composable
+fun EmptyHabitSummaryBox(size: Dp = 40.dp) {
+    Box(modifier = Modifier
+        .size(size)
+        .padding(4.dp)
+        .clip(RectangleShape)
+        .background(Color.Transparent)
     )
 }
--- android-client/app/src/main/java/com/duchastel/simon/habittracker/viewmodels/SummaryPageViewModel.kt
+++ android-client/app/src/main/java/com/duchastel/simon/habittracker/viewmodels/SummaryPageViewModel.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.duchastel.simon.habittracker.network.models.HabitSummary
 import com.duchastel.simon.habittracker.repositories.HabitsRepository
 import com.duchastel.simon.habittracker.repositories.IdentityRepository
 import kotlinx.coroutines.launch
@@ -24,12 +23,251 @@ class SummaryPageViewModel(
             val userId = identityRepository.userId()
             val habitsSummary = habitsRepository.getSummary(userId)
 
-            uiState = uiState.copy(isLoading = false, habits = habitsSummary)
+            uiState = uiState.copy(isLoading = false, habits = stubData)
         }
     }
 
     data class UIState(
         val isLoading: Boolean,
-        val habits: List<HabitSummary>,
+        val habits: List<HabitListElements>,
     )
+
+    sealed class HabitListElements {
+        data class MonthLabel(val month: String): HabitListElements()
+        data class Week(val habits: List<HabitStatus>): HabitListElements()
+    }
+
+    sealed class HabitStatus {
+        object Empty: HabitStatus()
+        data class Complete(val percent: Float): HabitStatus()
+        data class Incomplete(val percent: Float): HabitStatus()
+    }
 }
+
+val stubData = listOf(
+    SummaryPageViewModel.HabitListElements.MonthLabel("August"),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.3f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.2f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.6f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+    )),
+    SummaryPageViewModel.HabitListElements.MonthLabel("July"),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.3f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.2f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.6f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.MonthLabel("June"),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.3f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.2f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.6f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+    )),
+    SummaryPageViewModel.HabitListElements.MonthLabel("May"),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+    )),
+    SummaryPageViewModel.HabitListElements.MonthLabel("April"),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.3f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.2f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.6f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+    )),
+    SummaryPageViewModel.HabitListElements.MonthLabel("March"),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.3f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.2f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.6f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.3f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.2f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.6f),
+    )),
+    SummaryPageViewModel.HabitListElements.MonthLabel("February"),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.3f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.2f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.6f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+        SummaryPageViewModel.HabitStatus.Empty,
+        SummaryPageViewModel.HabitStatus.Empty,
+    )),
+    SummaryPageViewModel.HabitListElements.MonthLabel("January"),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+    SummaryPageViewModel.HabitListElements.Week(listOf(
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(1.0f),
+        SummaryPageViewModel.HabitStatus.Complete(0.7f),
+        SummaryPageViewModel.HabitStatus.Incomplete(0.2f),
+        SummaryPageViewModel.HabitStatus.Complete(0.0f),
+        SummaryPageViewModel.HabitStatus.Incomplete(1.0f),
+    )),
+)
+
